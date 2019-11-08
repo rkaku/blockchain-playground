3eaa77606992859a95a65fd43168cafdd62f2970
rkaku <greenapple728@icloud.com>
2019-11-08 14:47:55 +0900

    implement proof of work

--- ruby/cipher.rb
+++ ruby/cipher.rb
@@ -4,4 +4,4 @@ require_relative './helper.rb'
 
 block = {'a': 1, 'b': 2}
 
-puts OpenSSL::Digest.new('sha256').update(sort_dict_by_key(block).to_s)
+p OpenSSL::Digest.new('sha256').update(sort_dict_by_key(block).to_s).to_s.slice(0..2)
